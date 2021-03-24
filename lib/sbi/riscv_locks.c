9a7c161516b7cb7a6019c6facba54117b9c218cc
Christoph Muellner <cmuellner@linux.com>
2021-03-24 12:49:59 +0100

    spinlocks: Replace test-and-set locks by ticket locks
    Test-and-set locks don't provide fairness and are non-deterministic
    (w.r.t. the prediction of the future holder of a lock).
    This can be a performance problem in case of lock contention.
    
    Let's change the implementation to use ticket locks, which guarantee
    a fair locking order (FIFO ordering).
    
    Ticket locks have two counters 'owner' and 'next'.
    The counter 'owner' hold the ticket number of the current lock owner.
    The counter 'next' holds the latest free ticket number.
    
    The lock is free, if both counters are equal.
    To acquire the lock, one atomically increments the 'next' counter
    to obtain a new ticket.
    Afterwards the counter 'owner' is polled unitl it becomes equal to
    the new ticket.
    To release a lock, one atmically increments the 'owner' counter.
    
    The implementation uses a 32-bit wide struct, which consists of
    two 16-bit counters (owner and next).
    
    Signed-off-by: Christoph Muellner <cmuellner@linux.com>

--- lib/sbi/riscv_locks.c
+++ lib/sbi/riscv_locks.c
@@ -2,44 +2,80 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2019 Western Digital Corporation or its affiliates.
- *
- * Authors:
- *   Anup Patel <anup.patel@wdc.com>
+ * Copyright (c) 2021 Christoph Müllner <cmuellner@linux.com>
  */
 
 #include <sbi/riscv_barrier.h>
 #include <sbi/riscv_locks.h>
 
-int spin_lock_check(spinlock_t *lock)
+static inline int spin_lock_unlocked(spinlock_t lock)
 {
-	return (lock->lock == __RISCV_SPIN_UNLOCKED) ? 0 : 1;
+        return lock.owner == lock.next;
+}
+
+bool spin_lock_check(spinlock_t *lock)
+{
+	RISCV_FENCE(r, rw);
+	return !spin_lock_unlocked(*lock);
 }
 
 int spin_trylock(spinlock_t *lock)
 {
-	int tmp = 1, busy;
+	u32 tmp0, tmp1, tmp2;
+	u32 inc = 1u << (offsetof(spinlock_t, next));
 
 	__asm__ __volatile__(
-		"	amoswap.w %0, %2, %1\n" RISCV_ACQUIRE_BARRIER
-		: "=r"(busy), "+A"(lock->lock)
-		: "r"(tmp)
+		"1:	li	%1, -65536\n"	//tmp1 = 0xffff0000
+		"	lr.w.aq	%0, %3\n"	//tmp0 = *lock
+		"	slliw	%2, %0, 16\n"	//tmp2 = tmp0 << 16
+		"	and	%1, %0, %1\n"	//tmp1 = tmp0 & 0xffff0000
+		"	bne	%1, %2, 2f\n"	//return if tmp1 != tmp2 (already locked)
+		"	addw	%0, %0, %4\n"	//tmp0 = tmp0 + inc
+		"	sc.w.rl	%0, %0, %3\n"	//*lock = tmp0
+		"	bnez	%0, 1b\n"	//tmp0 == 0
+		"2:"
+		: "=&r"(tmp0), "=&r"(tmp1), "=&r"(tmp2), "+A"(*lock)
+		: "I"(inc)
 		: "memory");
 
-	return !busy;
+	return !tmp0;
 }
 
 void spin_lock(spinlock_t *lock)
 {
-	while (1) {
-		if (spin_lock_check(lock))
-			continue;
+	u32 tmp0, tmp1, tmp2;
+	u32 inc = 1u << (offsetof(spinlock_t, next));
 
-		if (spin_trylock(lock))
-			break;
-	}
+	__asm__ __volatile__(
+		/* Atomically increment the next ticket. */
+		"0:	lr.w.aq	%0, %3\n"	//tmp0 = *lock
+		"	addw	%0, %0, %4\n"	//tmp0 = tmp0 + inc
+		"	sc.w.rl	%0, %0, %3\n"	//*lock = tmp0
+		"	bnez	%0, 1b\n"	//tmp0 == 0
+
+		/* Did we get the lock? */
+		"1:	li	%1, -65536\n"	//tmp1 = 0xffff0000
+		"	slliw	%2, %0, 16\n"	//tmp2 = tmp0 << 16
+		"	and	%1, %0, %1\n"	//tmp1 = tmp0 & 0xffff0000
+		"	beq	%1, %2, 3f\n"	//return if tmp1 == tmp2 (we own the lock)
+		/* No, let's spin on the lock. */
+		"	lr.w.aq	%0, %3\n"	//tmp0 = *lock
+		"	j	1b\n"
+		"3:"
+		: "=&r"(tmp0), "=&r"(tmp1), "=&r"(tmp2), "+A"(*lock)
+		: "I"(inc)
+		: "memory");
 }
 
 void spin_unlock(spinlock_t *lock)
 {
-	__smp_store_release(&lock->lock, __RISCV_SPIN_UNLOCKED);
+	u32 inc = 1u << (offsetof(spinlock_t, owner) * sizeof(u8));
+
+	__asm__ __volatile__(
+		"	amoadd.w %0, %0, %1\n"
+		RISCV_RELEASE_BARRIER
+		: "=r"(inc), "+A"(*lock)
+		:
+		: "memory");
 }
+
